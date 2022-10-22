58e03d2b6d1fd05239b92e8047f6453f6f64802b
LeonardoPires2508 <ppires.leonardo@gmail.com>
2022-10-21 22:25:09 -0300

    atividades 01

---
+++ atividades/src/main/java/estruturas_de_decisao/parte01/Exercicio01.java
@@ -0,0 +1,9 @@
+package estruturas_de_decisao.parte01;
+
+public class Exercicio01 {
+    public static void main (String[] args){
+    int numero = 3;
+        System.out.println("O número é " + numero + ".");
+    }
+
+}
