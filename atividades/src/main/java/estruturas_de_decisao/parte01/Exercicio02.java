125af0d1e8455e0247cc0826bbf4d8cc485a0fa7
LeonardoPires2508 <ppires.leonardo@gmail.com>
2022-10-21 22:50:59 -0300

    atividades 01

---
+++ atividades/src/main/java/estruturas_de_decisao/parte01/Exercicio02.java
@@ -0,0 +1,9 @@
+package estruturas_de_decisao.parte01;
+
+public class Exercicio02 {
+    public static void main(String[] args){
+    double numero = 2.8;
+        System.out.println("O número "+numero+" é real.");
+    }
+
+}
---
+++ atividades/src/main/java/estruturas_de_decisao/parte01/Exercicio03.java
@@ -0,0 +1,18 @@
+package estruturas_de_decisao.parte01;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        System.out.println("Digite o primeiro numero: ");
+        Scanner numeros = new Scanner(System.in);
+        int numero1 = numeros.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int numero2 = numeros.nextInt();
+        System.out.println("Digite o terceiro numero: ");
+        int numero3 = numeros.nextInt();
+        System.out.println("======================");
+        int resultado = numero1 + numero2 + numero3;
+        System.out.println("A soma de: " +numero1+ " + " +numero2+ " + " +numero3+ " + " + " = " + resultado);
+    }
+}
